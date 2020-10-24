b40e83d7459dc6956e51b89d811ea5b049311454
lbratkovskaya <lbratkovskaya@naumen.ru>
2020-10-24 15:14:59 +0500

    feat: add weather and bcg change

--- momentum/script.js
+++ momentum/script.js
@@ -1,15 +1,44 @@
 // DOM Elements
-const time = document.querySelector('.time'),
+const dateTag = document.querySelector('.date'),
+  time = document.querySelector('.time'),
   greeting = document.querySelector('.greeting'),
   name = document.querySelector('.name'),
-  focus = document.querySelector('.focus');
+  cityTag = document.querySelector('.city'),
+  weatherIcon = document.querySelector('.weather-icon'),
+  weatherTemp = document.querySelector('.weather-temperature'),
+  weatherState = document.querySelector('.weather-state'),
+  blockquote = document.querySelector('blockquote'),
+  figcaption = document.querySelector('figcaption'),
+  nextImgBtn = document.querySelector('.next-image');;
 
 // Options
-const showAmPm = true;
+let readyToSwitch = true;
+let ingIndex = new Date().getHours() + 1;
+const dayPeriodsCount = 4;
+const apiKey = '2bd82ee7444fb5848885689e5988a2ca';
+
+const imageNamesArray = (() => {
+  let result = [];
+  for (i = 0; i < dayPeriodsCount; i++) {
+    let arr = [];
+    while (arr.length < 24 / dayPeriodsCount) {
+      let ind = Math.floor(Math.random() * 20) + 1;
+      if (!arr.includes(ind)) {
+        arr.push(ind);
+      }
+    }
+    result = [...result, ...arr];
+  }
+
+  return result;
+})();
 
 // Show Time
 function showTime() {
   let today = new Date(),
+    day = today.getDay(),
+    date = today.getDate(),
+    month = today.getMonth(),
     hour = today.getHours(),
     min = today.getMinutes(),
     sec = today.getSeconds();
@@ -17,15 +46,92 @@ function showTime() {
   // Set AM or PM
   const amPm = hour >= 12 ? 'PM' : 'AM';
 
-  // 12hr Format
-  hour = hour % 12 || 12;
+  // // 12hr Format
+  // hour = hour % 12 || 12;
+
+  formatAndFillDate(month, date, day);
 
   // Output Time
-  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
-    sec
-  )} ${showAmPm ? amPm : ''}`;
+  time.innerHTML = `<span>${addZero(hour)}</span><span>:</span><span>${addZero(min)}</span><span>:</span><span>${addZero(sec)}</span>`;
+
+  if (min < 1 && readyToSwitch) {
+    readyToSwitch = false;
+    switchBackgroundImage(hour);
+  } else if (!readyToSwitch) {
+    readyToSwitch = true;
+  }
 
   setTimeout(showTime, 1000);
+}
+
+// Add Zeros
+function formatAndFillDate(month, date, day) {
+  let monthName = '',
+    dayName = '';
+  switch (month) {
+    case 0:
+      monthName = 'января';
+      break;
+    case 1:
+      monthName = 'февраля';
+      break;
+    case 2:
+      monthName = 'марта';
+      break;
+    case 3:
+      monthName = 'апреля';
+      break;
+    case 4:
+      monthName = 'мая';
+      break;
+    case 5:
+      monthName = 'июня';
+      break;
+    case 6:
+      monthName = 'июля';
+      break;
+    case 7:
+      monthName = 'августа';
+      break;
+    case 8:
+      monthName = 'сентября';
+      break;
+    case 9:
+      monthName = 'октября';
+      break;
+    case 10:
+      monthName = 'ноября';
+      break;
+    case 11:
+      monthName = 'декабря';
+      break;
+  }
+
+  switch (day) {
+    case 0:
+      dayName = 'воскресенье';
+      break;
+    case 1:
+      dayName = 'понедельник';
+      break;
+    case 2:
+      dayName = 'вторник';
+      break;
+    case 3:
+      dayName = 'среда';
+      break;
+    case 4:
+      dayName = 'четверг';
+      break;
+    case 5:
+      dayName = 'пятница';
+      break;
+    case 6:
+      dayName = 'суббота';
+      break;
+  }
+
+  dateTag.innerHTML = `<span>${date} ${monthName}, </span><span>${dayName}</span>`;
 }
 
 // Add Zeros
@@ -38,24 +144,112 @@ function setBgGreet() {
   let today = new Date(),
     hour = today.getHours();
 
-  if (hour < 12) {
+  switchBackgroundImage(hour);
+}
+
+function switchBackgroundImage(hour) {
+  let greet = getGreetingByHour(hour);
+
+  let folderName = greet.folderName || greet.greetingStr.toLowerCase();
+  let urlString = `assets/images/${folderName}/${addZero(imageNamesArray[hour])}.jpg`;
+
+  greeting.textContent = `Good ${greet.greetingStr}, `;
+
+  // document.body.style.backgroundImage = `url(${urlString})`;
+
+  const img = document.createElement('img');
+  img.src = urlString;
+  img.onload = () => {
+    document.body.style.backgroundImage = `url(${urlString})`;
+  };
+}
+
+function getGreetingByHour(hour) {
+  let greetingStr = '',
+    folderName = '';
+
+  if (hour < 6) {
+    // Night
+    greetingStr = 'Night';
+    document.body.style.color = 'white';
+  } else if (hour < 12) {
     // Morning
-    document.body.style.backgroundImage =
-      "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
-    greeting.textContent = 'Good Morning, ';
+    greetingStr = 'Morning';
+
   } else if (hour < 18) {
     // Afternoon
-    document.body.style.backgroundImage =
-      "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
-    greeting.textContent = 'Good Afternoon, ';
+    greetingStr = 'Afternoon';
+    folderName = 'day';
   } else {
     // Evening
-    document.body.style.backgroundImage =
-      "url('https://i.ibb.co/924T2Wv/night.jpg')";
-    greeting.textContent = 'Good Evening, ';
     document.body.style.color = 'white';
+    greetingStr = 'Evening';
+  }
+
+  return { 'greetingStr': greetingStr, 'folderName': folderName };
+}
+
+function setQuote() {
+  const url = `https://quote-garden.herokuapp.com/api/v2/quotes/random`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      blockquote.innerText = data.quote.quoteText;
+      figcaption.innerText = data.quote.quoteAuthor;
+    });
+}
+
+function setCityAndWeather(ev) {
+  if (ev.type === 'keypress') {
+    if (ev.which == 13 || ev.keyCode == 13) {
+      if (ev.target.innerText) {
+        localStorage.setItem('userCity', ev.target.innerText);
+        updateWeather(ev.target.innerText);
+      } else {
+        cityTag.innerText = localStorage.getItem('userCity') || '[Enter City]'
+      }
+
+      cityTag.blur();
+      ev.preventDefault();
+    }
+  } else {
+    if (ev.target.innerText) {
+      localStorage.setItem('userCity', ev.target.innerText);
+      updateWeather(ev.target.innerText);
+    } else {
+      cityTag.innerText = localStorage.getItem('userCity') || '[Enter City]'
+    }
+  }
+
+}
+
+function getCityAndWeather() {
+  if (localStorage.getItem('userCity')) {
+    cityTag.textContent = localStorage.getItem('userCity');
+    updateWeather(localStorage.getItem('userCity'));
+  } else {
+    cityTag.textContent = '[Enter City]';
   }
 }
+
+function updateWeather(userCity) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&lang=en&appid=${apiKey}&units=metric`;
+  fetch(url)
+    .then((res) => {
+      if (200 === res.status) {
+        return res.json();
+      } else {
+        weatherTemp.innerHTML = "Sorry, error fetching";
+      }
+
+    })
+    .then((data) => {
+      weatherIcon.classList = `weather-icon owf owf-${data.weather[0].id}`;
+      weatherTemp.innerHTML = `${data.main.temp}&#176;C`;
+      weatherState.innerHTML = `<span>${data.weather[0].description}, </span><span>wind ${data.wind.speed} m/s, </span><span>hum. ${data.main.humidity}%</span>`;
+    });
+}
+
 
 // Get Name
 function getName() {
@@ -71,12 +265,46 @@ function setName(e) {
   if (e.type === 'keypress') {
     // Make sure enter is pressed
     if (e.which == 13 || e.keyCode == 13) {
-      localStorage.setItem('name', e.target.innerText);
+      if (e.target.innerText) {
+        localStorage.setItem('name', e.target.innerText);
+      } else {
+        e.target.textContent = localStorage.getItem('name') || '[Enter Name]';
+      }
       name.blur();
+      e.preventDefault();
     }
   } else {
-    localStorage.setItem('name', e.target.innerText);
+    if (e.target.innerText) {
+      localStorage.setItem('name', e.target.innerText);
+    } else {
+      e.target.textContent = localStorage.getItem('name') || '[Enter Name]';
+    }
   }
+}
+
+function setImage() {
+
+  const index = ingIndex % imageNamesArray.length;
+
+  let greet = getGreetingByHour(index);
+
+  let folderName = greet.folderName || greet.greetingStr.toLowerCase();
+
+  const imageSrc = `assets/images/${folderName}/${addZero(imageNamesArray[index])}.jpg`;
+
+  loadBgImage(imageSrc);
+  ingIndex++;
+  nextImgBtn.disabled = true;
+  setTimeout(function() { nextImgBtn.disabled = false }, 1000);
+}
+
+function loadBgImage(imgSrc) {
+  const src = imgSrc;
+  const img = document.createElement('img');
+  img.src = src;
+  img.onload = () => {
+    document.body.style.backgroundImage = `url(${src})`;
+  };
 }
 
 // Get Focus
@@ -103,11 +331,16 @@ function setFocus(e) {
 
 name.addEventListener('keypress', setName);
 name.addEventListener('blur', setName);
-focus.addEventListener('keypress', setFocus);
-focus.addEventListener('blur', setFocus);
+name.addEventListener('focus', () => (name.innerText = ''));
+cityTag.addEventListener('keypress', setCityAndWeather);
+cityTag.addEventListener('blur', setCityAndWeather);
+cityTag.addEventListener('focus', () => (cityTag.innerText = ''));
+
+nextImgBtn.addEventListener('click', setImage);
 
 // Run
 showTime();
 setBgGreet();
 getName();
-getFocus();
+getCityAndWeather();
+setQuote();
